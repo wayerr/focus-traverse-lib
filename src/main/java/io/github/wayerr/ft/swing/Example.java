016ce74160a62af220d55474659cab1db808f2b6
wayerr <radiofun@ya.ru>
2015-02-12 16:04:24 +0300

    more complex example

--- src/main/java/io/github/wayerr/ft/swing/Example.java
+++ src/main/java/io/github/wayerr/ft/swing/Example.java
@@ -9,6 +9,7 @@ import java.lang.reflect.InvocationTargetException;
  * Created by wayerr on 10.02.15.
  */
 public final class Example implements Runnable {
+    private static final int GAP = 3;
 
     public static void main(String args[]) throws InvocationTargetException, InterruptedException {
         Example example = new Example();
@@ -29,16 +30,15 @@ public final class Example implements Runnable {
         JComponent one = new JScrollPane(new JTextArea("1"));
         JComponent two = new JScrollPane(new JTextArea("2"));
         JComponent three = new JScrollPane(new JTextArea("3"));
-        JComponent four = new JScrollPane(new JTextArea("4"));
+        JComponent four = createPanel();
 
-        final int gap = 3;
         gl.setVerticalGroup(gl.createParallelGroup()
             .addGroup(gl.createSequentialGroup()
                 .addGroup(gl.createParallelGroup()
                     .addComponent(one, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
                     .addComponent(two, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
                 )
-                .addGap(gap)
+                .addGap(GAP)
                 .addComponent(three, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
             )
             .addComponent(four, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
@@ -47,13 +47,13 @@ public final class Example implements Runnable {
             .addGroup(gl.createParallelGroup()
                 .addGroup(gl.createSequentialGroup()
                     .addComponent(two, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
-                    .addGap(gap)
+                    .addGap(GAP)
                     .addComponent(one, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
                 )
                 .addComponent(three, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
             )
-            .addGap(gap)
-            .addComponent(four, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
+            .addGap(GAP)
+            .addComponent(four, 100, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
         );
 
 
@@ -66,6 +66,41 @@ public final class Example implements Runnable {
         double y = ss.height / 3d;
         frame.setBounds((int) x, (int) y, (int) ((ss.width - x) / 2d), (int) ((ss.height - y) / 2d));
         frame.setVisible(true);
+    }
+
+    private JComponent createPanel() {
+        JPanel panel = new JPanel();
+        GroupLayout gl = new GroupLayout(panel);
+        panel.setLayout(gl);
+
+        JComponent one = new JCheckBox("checkbox 1");
+        JComponent two = new JCheckBox("checkbox 2");
+        JComponent three = new JButton("ok");
+        JComponent four = new JButton("cancel");
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+            .addComponent(one)
+            .addGap(GAP)
+            .addComponent(two)
+            .addGap(GAP)
+            .addGroup(gl.createParallelGroup()
+                .addComponent(three)
+                .addComponent(four)
+            )
+        );
+        gl.setHorizontalGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(two, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
+            .addComponent(one, 0, GroupLayout.PREFERRED_SIZE, Integer.MAX_VALUE)
+            .addGroup(gl.createSequentialGroup()
+                .addGap(0, 10, Integer.MAX_VALUE)
+                .addComponent(three)
+                .addGap(GAP)
+                .addComponent(four)
+                .addGap(GAP)
+            )
+        );
+        gl.linkSize(three, four);
+        return panel;
     }
 
     private JMenuBar createMenuBar() {
